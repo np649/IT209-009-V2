157031e6098218bd25dee02951d6456dd822837c
np649@njit.edu <np649@njit.edu>
2021-12-04 16:24:56 -0500

    modified home.php to become the dashboard

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,15 +1,31 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 ?>
-<h1>Home</h1>
+<h1>Dashboard</h1>
 <?php
 
 if (is_logged_in(true)) {
-    echo "Welcome home, " . get_username();
+    echo "Login Confirmed" ;
     //comment this out if you don't want to see the session variables
     //echo "<pre>" . var_export($_SESSION, true) . "</pre>";
 }
 ?>
+
+<div class="jumbotron mt-4 text-center">
+      <h1 class="display-4">Welcome, <?php echo $_SESSION["user"]['username']; ?>!</h1>
+      <hr class="my-4">
+      <p class="lead">Choose an action</p>
+      <p>
+        <a class="btn btn-primary" href="#" role="button"> Create Account</a>
+        <a class="btn btn-primary" href="#" role="button"> My Accuonts</a>
+        <a class="btn btn-primary" href="#" role="button"> Deposit</a>
+        <a class="btn btn-primary" href="#" role="button"> Withdraw</a>
+        <a class="btn btn-primary" href="#" role="button"> Transfer</a>
+        <a class="btn btn-primary" href="#" role="button"> Profile</a>
+      </p>
+    </div>
+
+
 <?php
 require(__DIR__ . "/../../partials/flash.php");
 ?>
