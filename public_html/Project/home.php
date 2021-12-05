9a1700e5843113a17fb14223ea167c32deb9498e
np649@njit.edu <np649@njit.edu>
2021-12-04 20:21:37 -0500

    added transaction history page

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -17,7 +17,8 @@ if (is_logged_in(true)) {
       <p class="lead">Choose an action</p>
       <p>
         <a class="btn btn-primary" href="create_account.php" role="button"> Create Account</a>
-        <a class="btn btn-primary" href="accounts.php" role="button"> My Accuonts</a>
+        <a class="btn btn-primary" href="accounts.php" role="button"> My Accounts</a>
+        <a class="btn btn-primary" href="transaction_history.php" role="button"> Transaction History</a>
         <a class="btn btn-primary" href="#" role="button"> Deposit</a>
         <a class="btn btn-primary" href="#" role="button"> Withdraw</a>
         <a class="btn btn-primary" href="#" role="button"> Transfer</a>
